c1750e1c5bdaaa01bc771e9d07220d9c3ad88d66
Goth <71470101+SabrinaMonoSoraka@users.noreply.github.com>
2022-08-01 15:20:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -162,6 +162,7 @@ def finalizando():
     '''
 
 hor()
+print color("do not run main.py in root mode","red")
 print color(h1(),"yellow")
 print color('''         http 80/ Victim
          http 3000/ Attacker''',"red")
